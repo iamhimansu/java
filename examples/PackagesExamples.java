4009996b4ac521d594bb5706ff6b5a248136118f
Himanshu Raj Aman <himanshu.aman@samarth.ac.in>
2023-08-26 08:25:12 +0530

    Packages

---
+++ examples/PackagesExamples.java
@@ -0,0 +1,7 @@
+package examples;
+
+public class PackagesExamples {
+    public static void main(String[] args) {
+        System.out.println("Hello this class is inside examples packages");
+    }
+}
