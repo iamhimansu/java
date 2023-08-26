09bf85fa7935d813bdd30ae89e454a6feb27b173
Himanshu Raj Aman <himanshu.aman@samarth.ac.in>
2023-08-26 08:33:27 +0530

    Import packages

--- examples/PackagesExamples.java
+++ examples/PackagesExamples.java
@@ -4,6 +4,10 @@ package examples;
  * We create packages in Java using package "packagename"
  */
 public class PackagesExamples {
+    public static void packageName() {
+        System.out.println("examples");
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello this class is inside examples packages");
     }
--- pack1/ImportPackages.java
+++ pack1/ImportPackages.java
@@ -1,2 +1,12 @@
+package pack1;
+
+import examples.PackagesExamples;
+
+/**
+ * To import created packages we use the import keyword.
+ */
 public class ImportPackages {
+    public static void main(String[] args) {
+        PackagesExamples.packageName();
+    }
 }
