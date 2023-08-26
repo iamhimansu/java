f3e06ee9373d4e7a6966a865871f99a91a4f2c4d
Himanshu Raj Aman <himanshu.aman@samarth.ac.in>
2023-08-26 09:12:00 +0530

    Constructors

--- examples/AccessModifiers.java
+++ examples/AccessModifiers.java
@@ -2,11 +2,15 @@ package examples;
 
 /**
  * AccessModifiers are of 4 types
+ * <p></p>
  * 1. PUBLIC -> will be accessible to all classes in any packages
+ * <p></p>
  * 2. PRIVATE -> will be accessible only to the parent, CANNOT BE ACCESSED FROM OUTSIDE THE CLASSES, ALSO packages
+ * <p></p>
  * 3. PROTECTED -> will be accessible to the parent and the child classes of the same packages or child class of other packages
+ * <p></p>
  * 4. DEFAULT -> will only be accessible to the class of same packages
- * <p>
+ * <p></p>
  * By default, if no access modifiers are specified the default specifier is used by the java compiler
  */
 public class AccessModifiers {
---
+++ examples/Constructors.java
@@ -0,0 +1,16 @@
+package examples;
+
+/**
+ * Constructors are used to make an object and initialize it, thus giving the object an identity of real object
+ * <p></p>
+ * Constructors don't have return values
+ * <p></p>
+ * If no constructors are made then a default constructor is made by the java compiler
+ * <p></p>
+ * Any level of access modifiers can be used to create a constructor
+ */
+public class Constructors {
+    Constructors() {
+        System.out.println("Hello, I am initialized.");
+    }
+}
